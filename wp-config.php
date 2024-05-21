be6c4657b4b9fe2924f271b079013dd13f419d3a
Vaibhav Rathore <vaibhav@coloredcow.com>
2024-05-21 11:26:38 +0530

    latest wp

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
