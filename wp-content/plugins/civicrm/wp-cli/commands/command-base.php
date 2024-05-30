a659f31007ebc11c16ee52c380ca592f81654b47
pokhiii <pokhi@coloredcow.com>
2024-05-30 23:42:00 +0530

    debug

--- wp-content/plugins/civicrm/wp-cli/commands/command-base.php
+++ wp-content/plugins/civicrm/wp-cli/commands/command-base.php
@@ -365,6 +365,10 @@ abstract class CLI_Tools_CiviCRM_Command_Base extends \WP_CLI\CommandWithDBObjec
     // Run the command.
     $command = 'pushd ' . dirname($directory) . '; ' . "zip -rq {$destination} ./" . basename($directory) . '; popd';
     $process_run = WP_CLI::launch($command, $exit_on_error, $return_detailed);
+
+    WP_CLI::log(WP_CLI::colorize($command));
+
+
     if (0 !== $process_run->return_code) {
       WP_CLI::error(sprintf(WP_CLI::colorize('Failed to compress zip archive: %y%s.%n'), $this->zip_error_msg($process_run->return_code)));
     }
