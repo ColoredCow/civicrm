b3fd04a4585e7c7490980153cc1e64cbd8c11222
pokhiii <pokhi@coloredcow.com>
2024-06-03 17:29:41 +0530

    plugin scaffold

---
+++ wp-content/plugins/goonj-custom/goonj-custom.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Goonj Custom
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     goonj-custom
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Goonj_Custom
+ */
+
+// Your code starts here.
