1cf1518718f82028832dfebbc4ded1b6e3c55b1e
pokhiii <pokhi@coloredcow.com>
2024-06-03 17:54:32 +0530

    add js scaffolding

--- wp-content/plugins/goonj-custom/goonj-custom.php
+++ wp-content/plugins/goonj-custom/goonj-custom.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Goonj Custom
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/coloredcow/civicrm/
+ * Description:     Customizations done for Goonj.
+ * Author:          ColoredCow
+ * Author URI:      https://coloredcow.com
  * Text Domain:     goonj-custom
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +12,24 @@
  * @package         Goonj_Custom
  */
 
-// Your code starts here.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'GC_PLUGIN_FILE' ) ) {
+	define( 'GC_PLUGIN_FILE', __FILE__ );
+}
+
+add_action( 'wp_enqueue_scripts', 'gc_register_scripts' );
+function gc_register_scripts() {
+	wp_register_script(
+		'gc-source-tracking',
+		plugins_url( 'assets/js/source-tracking.js', GC_PLUGIN_FILE ),
+		array ('jquery' ),
+		'0.1.0',
+		false
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'gc_enqueue_scripts' );
+function gc_enqueue_scripts() {
+	wp_enqueue_script( 'gc-source-tracking' );
+}
